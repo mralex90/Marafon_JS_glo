4cc465f6519fbe0a49fe26da4a645c870bf07f45
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-27 22:20:28 +0300

    sending object
    {json} placeholder

--- Marafon_JS_Glo/js/sendForm.js
+++ Marafon_JS_Glo/js/sendForm.js
@@ -1,12 +1,17 @@
 const form = document.querySelector('.form-test-drive')
-
+// 'https://jsonplaceholder.typicode.com'
 form.addEventListener('submit', (event) => {
     event.preventDefault()
 
+    let data ={}
+
     for(let {name, value} of form.elements) {
-        
         if (name) {  
-            console.dir(value);
+            data[name] = value
         }
     }
+
+    fetch('https://jsonplaceholder.typicode.com/posts').then(response => {
+        console.log(response);
+    })
 })
